e8936808a624136527b16de6d5dc9bee79f05ecf
tsultan16 <tanzids@student.unimelb.edu.au>
2024-07-15 15:05:37 +1000

    added more helper functions

--- database/utils.py
+++ database/utils.py
@@ -19,6 +19,40 @@ import logging
 import re
 import json
 import xml.etree.ElementTree as ET
+import copy
+from collections import defaultdict
+
+
+tables_global = None
+pk_columns_dict = {}
+MAX_TABLE_SCAN_TIME_SAMPLES = 1000
+
+
+class Query:
+    def __init__(self, connection, template_id, query_string, payload, predicates, order_by, time_stamp=0, benchmark="TPC-H"):
+        self.template_id = template_id
+        self.query_string = query_string
+        self.payload = payload
+        self.predicates = predicates
+        self.order_by = order_by
+        self.benchmark = benchmark
+        self.group_by = {}
+        self.selectivity = estimate_selectivity(connection, query_string, predicates)
+        self.frequency = 1
+        self.first_seen = time_stamp
+        self.last_seen = time_stamp
+        self.table_scan_times = get_query_table_scan_times(connection, query_string, predicates)
+        self.index_scan_times = copy.deepcopy(self.table_scan_times)
+        self.context = None
+
+    def get_id(self):
+        return self.template_id
+
+    def __hash__(self):
+        return self.template_id
+
+    def __str__(self):
+        return f"template: {self.template}\n\query string: {self.query_string}\npayload: {self.payload}\npredicates: {self.predicates}\norder_bys: {self.order_bys}"
 
 
 
@@ -257,7 +291,7 @@ def get_nonclustered_indexes(connection):
 
 def remove_all_nonclustered_indexes(connection, verbose=False):
     # get all non-clustered indexes
-    indexes = get_nonclustered_indexes()
+    indexes = get_nonclustered_indexes(connection)
     print(f"All non-clustered indexes --> {indexes}")
     # drop all non-clustered indexes
     for (schema_name, table_name, index_name) in indexes:
@@ -281,5 +315,358 @@ def get_current_pds_size(connection):
         cursor.close()    
 
     return pds_size
+
+
+# get XML query plan for a given query
+def get_query_plan(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SET SHOWPLAN_XML ON")
+        cursor.execute(query)
+        plan_xml = cursor.fetchone()[0]
+        cursor.execute("SET SHOWPLAN_XML OFF")
+    except pyodbc.Error as e:
+        print(f"Error fetching query plan for query {query}: {e}")
+        plan_xml = None
+    finally:
+        cursor.close()    
+
+    return plan_xml
+
+
+# estimate the selectivity of a query, i.e. the fraction of rows from each table that satisfy the predicates so will be returned by the query
+def estimate_selectivity(connection, query, predicates, verbose=False):
+    # get query plan xml
+    plan_xml = get_query_plan(connection, query)
+    if plan_xml != "":
+        read_rows = {} 
+        selectivity = {}
+        query_plan = QueryPlan(plan_xml)
+        tables = predicates.keys()
+        if verbose: print(f"Tables: {tables}")
+        # get num rows scanned for each table scan
+        for table in tables:
+            read_rows[table] = 1000000000 # initialize to a large number, this is the cap for the number of rows scanned
+        
+        # look at the clustered index usage to get the number of rows scanned for each table
+        for index_scan in query_plan.clustered_index_usage:
+            if verbose:
+                print(f"Index Scan: {index_scan}")
+
+            if index_scan[0] not in read_rows:
+                read_rows[index_scan[0]] = read_rows[table]
+            else:
+                read_rows[index_scan[0]] = min(index_scan[5], read_rows[index_scan[0]])    
+
+        for table in tables:
+            table_row_count = get_table_row_count(connection, 'dbo', table)
+            selectivity[table] = min(1.0, read_rows[table] / table_row_count)
+            if verbose:
+                print(f"Table: {table}, Rows Scanned: {read_rows[table]}, table row count: {table_row_count}, selectivity: {selectivity[table]}")  
+ 
+
+    else:
+        # if query plan is not available, assume selectivity is 1, i.e. full table scan
+        selectivity = 1
+
+    return selectivity
+
+
+
+# execute a query and get the time taken to scan each table
+def get_query_table_scan_times(connection, query_string, predicates):
+    # execute the query and get the time taken to scan each table
+    table_scan_times = {}
+    elapsed_time, index_seeks, clustered_index_scans = execute_query(query_string, connection)
+    if clustered_index_scans:
+        for index_scan in clustered_index_scans:
+            table_name = index_scan[0]
+            if table_name not in table_scan_times:
+                table_scan_times[table_name] = []
+            # add the time taken to scan the table to the list of scan times (upto MAX_TABLE_SCAN_TIME_SAMPLES samples)
+            if len(table_scan_times[table_name]) < MAX_TABLE_SCAN_TIME_SAMPLES:
+                table_scan_times[table_name].append(index_scan[1])    
+
+    return table_scan_times
+
+
+# table object
+class Table:
+    def __init__(self, table_name, row_count, pk_columns):
+        self.table_name = table_name
+        self.row_count = row_count
+        self.pk_columns = pk_columns
+        self.columns = None
+
+    def set_columns(self, columns):
+        self.columns = columns
+
+    def get_columns(self):  
+        if self.columns is None:
+            raise Exception("Columns have not been set for this table yet.")                
+        return self.columns
+
+    def __str__(self):
+        return f"Table: {self.table_name}, Row Count: {self.row_count}, PK Columns: {self.pk_columns}"
+
+# column object
+class Column:
+    def __init__(self, table_name, column_name, column_type):
+        self.table_name = table_name    
+        self.column_name = column_name
+        self.column_type = column_type
+        self.column_size = None
+        self.max_column_size = None
+
+    def set_column_size(self, size):
+        self.column_size = size
+
+    def set_max_column_size(self, max_size):
+        self.max_column_size = max_size
+
+    def get_id(self):
+        return f"{self.table_name}_{self.column_name}"    
+    
+    @staticmethod
+    def construct_id(table_name, column_name):
+        return f"{table_name}_{column_name}"
+        
+    def __str__(self):
+        return f"Column: {self.column_name}, Column Type Type: {self.column_type}, Column Size: {self.column_size}, Max Column Size: {self.max_column_size}"
+
+
+# get number of rows in a given table
+def get_table_row_count(connection, schema_name, table_name):
+    cursor = connection.cursor()
+    query = f""" 
+                SELECT SUM (Rows)
+                FROM sys.partitions
+                WHERE index_id IN (0, 1) 
+                AND object_id = OBJECT_ID('{schema_name}.{table_name}')
+            """
+    try:
+        cursor.execute(query)
+        row_count = cursor.fetchone()[0]
+    except pyodbc.Error as e:
+        print(f"Error fetching row count for table {table_name}: {e}")
+        row_count = 0
+    finally:
+        cursor.close()    
+
+    return row_count
+
+
+# get primary key columns of a given table
+def get_primary_key_columns(connection, schema_name, table_name):
+    if table_name in pk_columns_dict:
+        pk_columns = pk_columns_dict[table_name]
+
+    else:
+        cursor = connection.cursor()
+        query = f"""
+                SELECT COLUMN_NAME
+                FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+                WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_SCHEMA + '.' + CONSTRAINT_NAME), 'IsPrimaryKey') = 1
+                AND TABLE_NAME = '{table_name}'
+                AND TABLE_SCHEMA = '{schema_name}'
+                """
+        try:
+            cursor.execute(query)
+            pk_columns = [result[0] for result in cursor.fetchall()]
+            pk_columns_dict[table_name] = pk_columns
+
+        except pyodbc.Error as e:
+            print(f"Error fetching primary key columns for table {table_name}: {e}")
+            pk_columns = []
+        finally:
+            cursor.close()    
+
+    return pk_columns
+
+
+# get list of all columns in a given table
+def get_table_columns(connection, schema_name, table_name):
+    columns = {}
+    cursor = connection.cursor()
+    try:
+        data_type_query = f"""
+                            SELECT COLUMN_NAME, DATA_TYPE, COL_LENGTH('{schema_name}.{table_name}', COLUMN_NAME)
+                            FROM INFORMATION_SCHEMA.COLUMNS
+                            WHERE TABLE_NAME = '{table_name}'
+                            """    
+        cursor.execute(data_type_query)
+        results = cursor.fetchall()
+        variable_len_query = 'SELECT '
+        variable_len_select_segments = []
+        variable_len_inner_segments = []
+        varchar_ids = []
+        for result in results:
+            col_name = result[0]
+            column = Column(table_name, col_name, result[1])
+            column.set_max_column_size(int(result[2]))
+            if result[1] != 'varchar':
+                column.set_column_size(int(result[2]))
+            else:
+                varchar_ids.append(col_name)
+                variable_len_select_segments.append(f'''AVG(DL_{col_name})''')
+                variable_len_inner_segments.append(f'''DATALENGTH({col_name}) DL_{col_name}''')
+            columns[col_name] = column
+
+        if len(varchar_ids) > 0:
+            variable_len_query = variable_len_query + ', '.join(
+                variable_len_select_segments) + ' FROM (SELECT TOP (1000) ' + ', '.join(
+                variable_len_inner_segments) + f' FROM {table_name}) T'
+            cursor.execute(variable_len_query)
+            result_row = cursor.fetchone()
+            for i in range(0, len(result_row)):
+                columns[varchar_ids[i]].set_column_size(result_row[i])
+    
+    except pyodbc.Error as e:
+        print(f"Error fetching columns for table {table_name}: {e}")
+    finally:
+        cursor.close()    
+
+    return columns
+
+
+# get list of all tables in the database
+def get_all_tables(connection, schema_name='dbo'):
+    global tables_global
+    if tables_global is not None:
+        return tables_global
+    
+    else:    
+        tables = {}
+        cursor = connection.cursor()
+        query = """
+                SELECT TABLE_NAME
+                FROM INFORMATION_SCHEMA.TABLES
+                WHERE TABLE_TYPE = 'BASE TABLE'
+                """
+        try:
+            cursor.execute(query)
+            results = cursor.fetchall()
+            for result in results:
+                table_name = result[0]
+                row_count = get_table_row_count(connection, schema_name, table_name)
+                pk_columns = get_primary_key_columns(connection, schema_name, table_name)
+                tables[table_name] = Table(table_name, row_count, pk_columns)
+                tables[table_name].set_columns(get_table_columns(connection, schema_name, table_name))
+
+        except pyodbc.Error as e:
+            print(f"Error fetching tables: {e}")
+        finally:
+            cursor.close()
+
+        tables_global = tables    
+        return tables
+    
+
+# get list of all columns in the database
+def get_all_columns(connection):
+    cursor = connection.cursor()
+    query = """     
+            SELECT TABLE_NAME, COLUMN_NAME 
+            FROM INFORMATION_SCHEMA.COLUMNS;
+            """
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        columns = defaultdict(list)
+        for result in results:
+            columns[result[0]].append(result[1])
+        num_columns = len(results)        
+
+    except pyodbc.Error as e:
+        print(f"Error fetching columns: {e}")
+        columns = None
+        num_columns = 0
+    finally:
+        cursor.close()
+
+    return columns, num_columns
+
+
+
+def get_index_id(index_cols, table_name, include_cols=()):
+    if include_cols:
+        include_col_names = '_'.join(tuple(map(lambda x: x[0:4], include_cols))).lower()
+        index_id = 'IXN_' + table_name + '_' + '_'.join(index_cols).lower() + '_' + include_col_names
+    else:
+        index_id = 'IX_' + table_name + '_' + '_'.join(index_cols).lower()
+    
+    # truncate to 128 chars
+    index_id = index_id[:127]
+    return index_id
+
+
+# estimate the size of an index on a given table
+def get_estimated_index_size(connection, table_name, columns, schema_name='dbo'):
+    # get table object
+    table = get_all_tables(connection)[table_name]
+    # get primary key columns
+    pk_columns = get_primary_key_columns(connection, schema_name, table_name)
+    # get size of primary key columns
+    pk_size = get_column_data_size(connection, table_name, pk_columns)
+    # get non primary key columns and calculate their sizes
+    col_not_pk = tuple(set(columns) - set(pk_columns))
+    col_not_pk_sizes = get_column_data_size(connection, table_name, col_not_pk)
+    # calculate size of each row in bytes
+    header_size_bytes = 6
+    nullable_buffer_bytes = 1
+    index_row_size = header_size_bytes + pk_size + col_not_pk_sizes + nullable_buffer_bytes
+    # get row count of table
+    row_count = table.row_count
+    # get estimated size of index in bytes
+    estimated_size = row_count * index_row_size
+    # convert to MB
+    estimated_size = estimated_size/float(1024*1024)
+    max_column_size = get_max_column_data_size(connection, table_name, columns)
+    # check if index size is going past 1700 MB
+    if max_column_size > 1700:
+        logging.info(f'Index size going past 1700 MB: {columns}')
+        estimated_size = 99999999
+    logging.debug(f"{columns} : {estimated_size}")
+
+    return estimated_size
+
+
+# compute column data size
+def get_column_data_size(connection, table_name, columns):
+    tables = get_all_tables(connection)
+    varchar_count = 0
+    column_data_size = 0
+
+    for column_name in columns:
+        column = tables[table_name].columns[column_name]
+        if column.column_type == 'varchar':
+            varchar_count += 1
+        column_data_size += column.column_size if column.column_size else 0
+        
+    if varchar_count > 0:
+        variable_key_overhead = 2 + 2 * varchar_count
+        return column_data_size + variable_key_overhead
+    else:
+        return column_data_size    
+    
+
+# compute maximum possible column data size
+def get_max_column_data_size(connection, table_name, columns):
+    tables = get_all_tables(connection)
+    column_data_size = 0
+    for column_name in columns:
+        column = tables[table_name].columns[column_name]
+        column_data_size += column.max_column_size if column.max_column_size else 0
+    return column_data_size
+
+
+
+
+
+
+
+
+
+
 
 
