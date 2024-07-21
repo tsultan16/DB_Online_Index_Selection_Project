55830d28cea61cb317891cfed69ed8538191325c
tsultan16 <tanzids@student.unimelb.edu.au>
2024-07-21 16:11:08 +1000

    implemented super arm selection and mab parameters update

--- MAB/mab.py
+++ MAB/mab.py
@@ -8,6 +8,7 @@ import os
 import sys
 from tqdm import tqdm
 import itertools
+from collections import defaultdict
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 target_subdirectory_path = os.path.join(script_directory, 'database')
@@ -31,7 +32,7 @@ class Index:
         self.clustered_index_time = None
         self.context_vector_columns = None
         self.payload_only = payload_only
-        self.index_usage_last = 0 
+        self.average_observed_reward = 0 
 
     def __str__(self):
         return f"Index({self.table_name}, {self.index_id}, {self.index_columns}, {self.include_columns}, {self.size}, {self.value})"
@@ -39,13 +40,40 @@ class Index:
 
 
 class MAB:
-    def __init__(self):
-        pass
+    def __init__(self, alpha=1.0, vlambda=0.5):
+        # define Lin UCB parameters
+        self.alpha = alpha     # UCB exploration parameter
+        self.vlambda = vlambda # regularization parameter
 
+        # get all columns
+        connection = start_connection()
+        self.all_columns, self.num_columns = get_all_columns(connection)
+        close_connection(connection)
 
+        self.context_size = self.num_columns + 2  # columns + derived
+        self.columns_to_idx = {}
+        i = 0
+        for table_name, columns in self.all_columns.items():
+            for column in columns:
+                self.columns_to_idx[column] = i
+                i += 1
+
+        self.idx_to_columns = {v: k for k, v in self.columns_to_idx.items()}   
+
+        # initialize matrix V and vector b
+        self.V = np.eye(self.context_size) * self.vlambda
+        self.b = np.zeros(shape=(self.context_size, 1))
+        self.context_vectors = None
+        self.upper_bounds  = None
+        self.index_selection_count = defaultdict(int)
+        
+
+    """ 
+        Candidate index generation
     """
-        Given a query, generate candidate indices based on the predicates and payload columns in the query.
-    """
+
+    
+    # Given a query, generate candidate indices based on the predicates and payload columns in the query.
     def generate_candidate_indices_from_predicates(self, connection, query, MAX_COLUMNS=6, SMALL_TABLE_IGNORE=10000, TABLE_MIN_SELECTIVITY=0.2, verbose=False):
         # get all tables in the db
         tables = get_all_tables(connection)
@@ -164,9 +192,7 @@ class MAB:
 
 
 
-    """
-        Given a miniworkload, which is a list of query objects, generate candidate indices
-    """
+    # Given a miniworkload, which is a list of query objects, generate candidate indices
     def generate_candidate_indices(self, connection, miniworkload, verbose=False):
         print(f"Gnereting candidate indices for {len(miniworkload)} queries...")
         index_arms = {} 
@@ -210,16 +236,141 @@ class MAB:
     def generate_context_vector_columns(self, index_arms, columns_to_idx):
         # stack up the context vectors for all indices into a single matrix
         context_vectors = np.vstack([self.generate_context_vector_columns_index(index, columns_to_idx) for index in index_arms.values()])
-
+        
         return context_vectors
 
 
     # generate derived piece
-    def generate_context_vector_derived(self, connection, index_arms):
+    def generate_context_vector_derived(self, connection, index_arms, selected_indices_last_round):
         database_size = get_database_size(connection)
         derived_context_vectors = np.zeros((len(index_arms), 2), dtype=float)
-        for i, index in enumerate(index_arms.values()):
-            derived_context_vectors[i,0] =  index.index_usage_last
-            derived_context_vectors[i,1] =  index.size/database_size
+        
+        for i, index in enumerate(index_arms.values()):    
+            # the first derived context component will be the average observed reward for this index
+            derived_context_vectors[i,0] =  index.average_observed_reward
+            
+            # the second derived context component will be used to estimate index creation cost as a linear function of index usage
+            # i.e. estimated index creation cost is the index size multiplied by a corresponding weight/parameter
+            if index.index_id not in selected_indices_last_round:
+                # if the index was not selected in the last round then it will be created, otherwise assigned a value of 0    
+                derived_context_vectors[i,1] =  index.size/database_size
         
         return derived_context_vectors
+
+
+    # generate context vectors (selected_indices_last_round should be a list of index ids)
+    def generate_contexts(self, connection, index_arms, selected_indices_last_round=[]):
+        columns_context_vectors = self.generate_context_vector_columns(index_arms, self.columns_to_idx)
+        derived_context_vectors = self.generate_context_vector_derived(connection, index_arms, selected_indices_last_round)
+
+        # concatenate the derived and column context vectors
+        return np.hstack((derived_context_vectors, columns_context_vectors))
+    
+
+    """ 
+        Selection of best configuration/super-arm
+            Here, we use LinUCB algorithm to select the best configuration: 
+                * first estimate the upper bound for the estimated expected reward for each index
+                * then (approximately) solve 0-1 knapsack problem to select a subset of indices which
+                  maximizes estimated total expected reward while satisfying constraint on config memory budget 
+
+    """
+    def select_best_configuration(self, context_vectors, index_arms, max_config_size_MB=1024, creation_cost_reduction_factor=3, verbose=False):
+        self.context_vectors = context_vectors
+        V_inv = np.linalg.inv(self.V)
+        
+        # compute parameters vector
+        theta = V_inv @ self.b 
+        # rescale the parameter corresponding to the size of the index
+        theta[1] = theta[1]/creation_cost_reduction_factor  
+        # estimate the expected reward upper bound for each arm/index
+        expected_reward = (context_vectors @ theta).reshape(-1)
+        # estimate upper confidence bound
+        confidence_bounds = self.alpha * np.sqrt(np.diag(context_vectors @ V_inv @ context_vectors.T))
+        # confidence_bounds = self.alpha * np.sqrt(np.einsum('ij,jk,ik->i', context_vectors, V_inv, context_vectors))
+        self.upper_bounds = expected_reward + confidence_bounds   
+        if verbose:
+            print(f"expected rewards shape {expected_reward.shape}")
+            print(f"confidence bounds shape {confidence_bounds.shape}")
+            #print(f"Expected reward upper bounds: {self.upper_bounds}")
+
+        # solve knapsack problem to select the best configuration
+        selected_indices = self.knapsack_solver(index_arms, max_config_size_MB, verbose)
+
+        # update the index selection count
+        for index in selected_indices:
+            self.index_selection_count[index.index_id] += 1
+
+        return selected_indices
+
+
+    # greedy 1/2 approximation algorithm for 0-1 knapsack problem     
+    def knapsack_solver(self, index_arms, max_config_size_MB, verbose=False):
+        # compute the ratio of the upper bound to the size of the index
+        ratios = self.upper_bounds / np.array([index.size for index in index_arms.values()])
+        if verbose:
+            print(f"Ratios shape: {ratios.shape}")
+
+        # sort the indices in descending order of ratio
+        sorted_indices = np.argsort(ratios)[::-1] 
+        # Extract the sorted index_arms values based on sorted_indices
+        sorted_index_arms = [list(index_arms.values())[i] for i in sorted_indices]
+        # select the indices that fit within the memory budget
+        selected_indices = []
+        current_memory_usage = 0
+        for index in sorted_index_arms:
+            index_size = index.size
+            if current_memory_usage + index_size <= max_config_size_MB:
+                selected_indices.append(index)
+                current_memory_usage += index_size
+            else:
+                break    
+
+        return selected_indices        
+
+
+    # updates matrix V and vector b based on the observed rewards
+    def update_parameters(self, selected_indices, observed_rewards, candidate_index_arms):
+        index_name_to_idx = {index.index_id: i for i, index in enumerate(candidate_index_arms)}
+        for index in selected_indices:
+            # get the context vector for this index
+            context_vector = self.context_vectors[index_name_to_idx[index.index_id]]
+            # get the observed reward for this index
+            if index.index_id in observed_rewards:
+                index_reward = observed_rewards[index.index_id]
+            else:
+                index_reward = (0, 0)
+
+            # update the moving average observed reward for this index over all rounds
+            candidate_index_arms[index.index_id].average_observed_reward = (index_reward[0] + candidate_index_arms[index.index_id].average_observed_reward)/2
+
+            # separate out the update for the index creation cost/reward component
+            temp_context = np.zeros_like(context_vector)
+            temp_context[1] = context_vector[1]
+            context_vector[1] = 0
+
+            # update the matrix V and vector b for non-index creation reward component
+            self.V += np.outer(context_vector, context_vector)
+            self.b += index_reward[0] * context_vector.reshape(-1,1)
+
+            # update the matrix V and vector b for index creation cost/reward component
+            # this is done separately to avoid the index creation cost/reward component from dominating the update
+            self.V += np.outer(temp_context, temp_context)
+            self.b += index_reward[1] * temp_context.reshape(-1,1)             
+            
+        # reset the context vectors and upper bounds
+        self.context_vectors = None
+        self.upper_bounds  = None
+
+
+
+
+
+
+
+
+
+
+
+
+
